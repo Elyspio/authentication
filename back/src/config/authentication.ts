3dc96f1acb50bdd8de52e97cb2a185110e090cfe
Jonathan Guichard <jona.guich69@gmail.com>
2021-10-05 00:06:18 +0200

    Back -- Feature -- Update TSED version

--- back/src/config/authentication.ts
+++ back/src/config/authentication.ts
@@ -3,7 +3,7 @@ import {env} from "process"
 import isDev = Helper.isDev;
 
 export const authorization_cookie_token = "authentication-token";
-export const authorization_cookie_username = "authentication_login";
+export const authorization_cookie_username = "authentication-login";
 const token_timeout = env.TOKEN_EXPIRATION;
 export const token_expiration = (token_timeout ? Number.parseInt(token_timeout) : 60) * 60 * 1000; // 60 mn
 export const tokenDomains = isDev() ? ["http://127.0.0.1:3000", "http://127.0.0.1:3001"] : ["https://elyspio.fr"]
--- back/src/web/decorators/protected.ts
+++ back/src/web/decorators/protected.ts
@@ -1,16 +1,23 @@
-import {In, JsonParameterTypes, Returns} from "@tsed/schema";
-import {UseAuth} from "@tsed/common";
-import {useDecorators} from "@tsed/core";
-import {Forbidden} from "@tsed/exceptions/lib/clientErrors"
-import {authorization_cookie_token} from "../../config/authentication";
-import {RequireLogin} from "../middleware/authentication";
-
+import { In, JsonParameterTypes, Returns } from "@tsed/schema";
+import { UseAuth } from "@tsed/common";
+import { useDecorators } from "@tsed/core";
+import { Forbidden } from "@tsed/exceptions";
+import { authorization_cookie_token } from "../../config/authentication";
+import { RequireLogin } from "../middleware/authentication";
 
 export function Protected(): Function {
-	return useDecorators(
-		UseAuth(RequireLogin),
-		In(JsonParameterTypes.HEADER).Name(authorization_cookie_token).Type(String).Required(false).Description("Authorization in header"),
-		In(JsonParameterTypes.COOKIES).Name(authorization_cookie_token).Type(String).Required(false).Description("Authorization in cookie"),
-		Returns(403, Forbidden).Description("You are not logged")
-	);
+  return useDecorators(
+    UseAuth(RequireLogin),
+    In(JsonParameterTypes.HEADER)
+      .Name(authorization_cookie_token)
+      .Type(String)
+      .Required(false)
+      .Description("Authorization in header"),
+    In(JsonParameterTypes.COOKIES)
+      .Name(authorization_cookie_token)
+      .Type(String)
+      .Required(false)
+      .Description("Authorization in cookie"),
+    Returns(403, Forbidden).Description("You are not logged")
+  );
 }
